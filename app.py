ff1e871be666ac283c6f88f969329de2d123c6b4
Maksym Sarana <maksym@verbit.ai>
2023-06-25 23:56:21 +0300

    template

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from flask import render_template
 
 server = Flask(__name__)
 
+@app.route('/')
+def hello(name=None):
+    return render_template('index.html', name=name)
 
 if __name__ == '__main__':
     server.run()
