7942299163de8e91e642863b0f3c0606a4dbb3cf
Maksym Sarana <maksym@verbit.ai>
2023-06-27 10:16:32 +0300

    show image

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import render_template
 from PIL import Image
 from io import BytesIO
 from model.predictor import KeyPointPredictor
+from base64 import b64encode
+
 
 app = Flask(__name__)
 predictor = KeyPointPredictor()
@@ -20,7 +22,11 @@ def upload_file():
         image = Image.open(BytesIO(f.read()))
         predictor.add_points(image)
 
-        return 'file uploaded successfully'
+        modified_image = BytesIO()
+        image.save(modified_image, format='PNG')
+        dataurl = 'data:image/png;base64,' + b64encode(modified_image.getvalue()).decode('ascii')
+
+        return render_template('image.html', image_data=dataurl)
 
 
 if __name__ == '__main__':
