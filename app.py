d40661c762e7c86842610cc143a278491876ce67
Maksym Sarana <maksym@verbit.ai>
2023-07-10 00:51:11 +0300

    reorganize templates

--- app.py
+++ app.py
@@ -5,14 +5,60 @@ import imageio.v3 as iio
 import numpy as np
 from PIL import Image
 from flask import Flask, request, render_template
+from enum import Enum
 
 from model.predictor import KeyPointPredictor
+
+MIN_IMAGE_SIZE = 200
+MAX_IMAGE_SIZE = 1000
+
+class MediaType(Enum):
+    IMAGE = 1
+    VIDEO = 2
+
 
 app = Flask(__name__, static_folder="static")
 predictor = KeyPointPredictor(model_file='artifacts/mixed_model_weights.pkl')
 
 
+def get_image_shape(h, w):
 
+    min_size = min(h, w)
+    max_size = max(h, w)
+
+    if min_size <= MIN_IMAGE_SIZE:
+        scale = MIN_IMAGE_SIZE / min_size
+        return int(h * scale), int(w * scale)
+    elif max_size >= MAX_IMAGE_SIZE:
+        scale = MAX_IMAGE_SIZE / max_size
+        return int(h * scale), int(w * scale)
+    else:
+        return h, w
+
+
+def process_image(f, small_model=False):
+    image = Image.open(BytesIO(f.read()))
+    h, w = get_image_shape(image.height, image.width)
+
+    predictor.process_image(image, small_model=small_model)
+
+    modified_media_bytes = BytesIO()
+    image.save(modified_media_bytes, format='PNG')
+    output_mimetype = 'image/png'
+    return f"data:{output_mimetype};base64,{b64encode(modified_media_bytes.getvalue()).decode('ascii')}", h, w
+
+
+def process_video(f, small_model=False):
+    fb = f.read()
+    extension = '.' + f.mimetype.split('/', maxsplit=1)[-1]
+    frames = iio.imread(fb, extension=extension)
+    fps = iio.immeta(fb, extension=extension)['fps']
+
+    frames_changed = np.array(
+        [np.array(image) for image in predictor.process_frames(frames, small_model=small_model)])
+    modified_media_bytes = iio.imwrite('<bytes>', frames_changed, extension='.mp4', fps=fps)
+    output_mimetype = 'video/mp4'
+    return f"data:{output_mimetype};base64,{b64encode(modified_media_bytes.getvalue()).decode('ascii')}"
 
 
 @app.route('/')
@@ -26,49 +72,15 @@ def submit_media():
     small_model = (request.form.get('model_selector') == 'small')
 
     if f.mimetype.startswith('image/'):
-        image = Image.open(BytesIO(f.read()))
-
-        predictor.process_image(image)
-
-        modified_media_bytes = BytesIO()
-        image.save(modified_media_bytes, format='PNG')
-        output_mimetype = 'image/png'
+        output_media, h, w = process_image(f, small_model)
+        return render_template('image.html', image_data=output_media, image_height=h, image_width=w)
 
     elif f.mimetype.startswith('video/'):
-        fb = f.read()
-        extension = '.' + f.mimetype.split('/', maxsplit=1)[-1]
-        frames = iio.imread(fb, extension=extension)
-        fps = iio.immeta(fb, extension=extension)['fps']
+        output_media = process_video(f, small_model)
+        return render_template('video.html', video_data=output_media)
 
-        frames_changed = np.array(
-            [np.array(image) for image in predictor.process_frames(frames, small_model=small_model)])
-        modified_media_bytes = iio.imwrite('<bytes>', frames_changed, extension='.mp4', fps=fps)
-        output_mimetype = 'video/mp4'
     else:
         raise NotImplementedError(f"Mimetype {f.mimetype} is not implemented")
-
-    return f"data:{output_mimetype};base64,{b64encode(modified_media_bytes.getvalue()).decode('ascii')}"
-
-
-@app.route('/video_uploader', methods=['GET', 'POST'])
-def upload_video():
-    if request.method == 'POST':
-        f = request.files['file']
-        if f.mimetype.startswith('video/'):
-            extension = '.' + f.mimetype.split('/', maxsplit=1)[-1]
-
-            fb = f.read()
-            frames = iio.imread(fb, extension=extension)
-            fps = iio.immeta(fb, extension=extension)['fps']
-
-            frames_changed = np.array([np.array(image) for image in predictor.process_frames(frames, small_model=False)])
-            fb_changed = iio.imwrite('<bytes>', frames_changed, extension='.mp4', fps=fps)  #TODO: extension?
-
-            dataurl = 'data:video/mp4;base64,' + b64encode(fb_changed).decode('ascii')
-
-            return render_template('video.html', video_data=dataurl)
-        else:
-            return "Error: You should submit a video file"
 
 
 if __name__ == '__main__':
